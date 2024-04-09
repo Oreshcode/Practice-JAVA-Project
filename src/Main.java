b47ca988565a9a2eb16849189e3363316cec6b28
Oreshcode <166463373+Oreshcode@users.noreply.github.com>
2024-04-09 23:09:01 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import Frame.MainFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setVisible(true);
+
+    }
+}
+// Starting API link
+// https://jobicy.com/api/v2/remote-jobs?count=20&geo=usa&industry=marketing&tag=seo
+
+// TO DO
+// https://jobi.cy/t8g32 <- add this to drop down menu
+// https://jobi.cy/veq2t Industry Slug drop down
+// Add DetailsPanel stuff
+
+
+// Panel class (HomePanel)
+// Data provider class (HomeDataProvider)
+// Coordinator class (HomeCoordinator)
+// Service class (HomeAPIService)
